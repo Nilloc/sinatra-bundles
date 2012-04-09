096d44367f6362e78515cbdfb169e7b4bb4fc71d
Collin Reisdorf <collin@paperclipped.com>
2012-04-09 12:55:07 -0300

    Adding plural versions of tag helpers to output individual css & js files for debugging.

--- lib/sinatra/bundles/helpers.rb
+++ lib/sinatra/bundles/helpers.rb
@@ -11,13 +11,15 @@ module Sinatra
         settings.javascript_bundles[bundle].to_html(sn)
       end
       
-      # Emit a script tag for each files in a javascript bundle
+      # Emit a script tag for each file in a javascript bundle
       #
       # @param [Symbol,String] bundle The bundle name
       # @return [String] HTML script tag
       def javascript_bundle_include_tags(bundle)
         sn = request.script_name rescue nil
-        settings.javascript_bundles[bundle].files
+        files = settings.javascript_bundles[bundle].files.map do |file|
+          "<script src=\"/javascripts/#{file}.js\" type=\"text/javascript\"></script>\n"
+        end
       end
 
       # Emit a script tag for a stylesheet bundle
@@ -27,6 +29,17 @@ module Sinatra
       def stylesheet_bundle_link_tag(bundle, media = :all)
         sn = request.script_name rescue nil
         settings.stylesheet_bundles[bundle].to_html(media, sn)
+      end
+      
+      # Emit a script tag for each file in a stylesheet bundle
+      #
+      # @param [Symbol,String] bundle The bundle name
+      # @return [String] HTML link tag
+      def stylesheet_bundle_link_tags(bundle, media = :all)
+        sn = request.script_name rescue nil
+        settings.stylesheet_bundles[bundle].files.map do |file|
+          "<link type=\"text/css\" href=\"/stylesheets/#{file}.css\" rel=\"stylesheet\" media=\"all\">\n"
+        end
       end
     end
   end
